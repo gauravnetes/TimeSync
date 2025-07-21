6beab3d6c43095ee854af28609ee3564bfa3282d
gauravnetes <gouravchandra935@gmail.com>
2025-07-22 00:04:21 +0530

    notFound page rename 404.js

--- app/404.js
+++ app/404.js
@@ -1,6 +1,6 @@
 'use client'
 
-export default function NotFound() {
+export default function Custom404() {
     return (
         <div className="text-4xl font-extrabold w-screen pt-80 grid justify-center">
             <h1>404 - Page Not Found</h1>
